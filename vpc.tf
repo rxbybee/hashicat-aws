e5e9a22a51106df6bcf5a436c8a794d5492fc778
rxbybee <bob.bybee@phoenix.edu>
2020-04-10 15:28:36 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,4 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
-
-  tags = {
-    Name = "main"
-  }
+module "vpc" {
+  source  = "app.terraform.io/bob-bybee/vpc/aws"
+  version = "2.33.0"
 }
